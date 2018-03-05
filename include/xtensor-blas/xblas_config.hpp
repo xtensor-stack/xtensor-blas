fe262ebb7d4bb114f1f6bba48f7033dce9cc4c05
Wolf Vollprecht <w.vollprecht@gmail.com>
2018-03-05 17:37:32 +0100

    add missing xblas_config.hpp

---
+++ include/xtensor-blas/xblas_config.hpp
@@ -0,0 +1,33 @@
+/***************************************************************************
+* Copyright (c) 2016, Johan Mabille and Sylvain Corlay                     *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#ifndef XBLAS_CONFIG_HPP
+#define XBLAS_CONFIG_HPP
+
+#define XTENSOR_BLAS_VERSION_MAJOR 0
+#define XTENSOR_BLAS_VERSION_MINOR 8
+#define XTENSOR_BLAS_VERSION_PATCH 0
+
+#ifndef USE_CXXLAPACK
+#define USE_CXXLAPACK
+#endif
+
+#ifndef BLAS_IDX
+#define BLAS_IDX int
+#endif
+
+#ifdef __CLING__
+#include "xtensor-blas/xtensor_config_cling.hpp"
+#endif
+
+namespace xt
+{
+    using blas_index_t = BLAS_IDX;
+}
+
+#endif
